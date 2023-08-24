aba3c827d18c570b2aa7f4a0312f682cfd6737f0
Willon Ferreira <contatowillon@gmail.com>
2023-08-24 16:41:21 -0400

    Add production enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
+  production: true,
   urlBase: `http://${location.hostname}:${9200}`,
-  collection: '/tests',
+  collection: '/production',
   searchRoute: '/_search'
 };
